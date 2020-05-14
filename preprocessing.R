c4b5434a4f1c0561e91ce8000b99b170e2950daa
LukasBudach <lukas.budach@student.hpi.de>
2020-05-14 14:11:56 +0200

    refactor preprocessing scripts

--- preprocessing.R
+++ preprocessing.R
@@ -3,37 +3,14 @@
 # Created by: lukas
 # Created on: 13/05/2020
 
-library(readr)
 
-col_names <- c('Index', 'SpeechDbId', 'Date', 'Period', 'Sitting', 'DocDbId', 'Speaker', 'Party', 'InterjectionCount', 'InterjectionContent', 'ParagraphCount', 'Speech')
-cols <- readr::cols(Index=col_integer(),
-                    SpeechDbId=col_integer(),
-                    Date=col_date(),
-                    Period=col_integer(),
-                    Sitting=col_integer(),
-                    DocDbId=col_integer(),
-                    Speaker=col_character(),
-                    Party=col_character(),
-                    InterjectionCount=col_integer(),
-                    InterjectionContent=col_character(),
-                    ParagraphCount=col_integer(),
-                    Speech=col_character())
+# random stuff
+for(i in seq(min(data$Period), max(data$Period))) {
+  print(sprintf('Period: %d speeches: %d', i, nrow(filter_period(data, i))))
+}
 
-data <- readr::read_csv('../coal-discourse/datasets/database_export_search_89.csv', skip=1, col_names=col_names, col_types=cols)
-
-# summary(data)
-# plot(table(data$ParagraphCount))
-
-# speech <- data[[13, "Speech"]]
-
-
-corpus <- tm::VCorpus(tm::VectorSource(data$Speech))
-corpus <- tm::tm_map(corpus, tm::content_transformer(tolower)) # MAKES EVERYTHING LOWERCASE
-corpus <- tm::tm_map(corpus, tm::removeNumbers) # REMOVE NUMBERS
-corpus <- tm::tm_map(corpus, tm::stripWhitespace) # REMOVE EXTRA WHITE SPACE
-freq_mat <- tm::TermDocumentMatrix(corpus)
-sparce_mat <- tm::removeSparseTerms(freq_mat, 0.996) # 0.9997
-freq <- as.matrix(sparce_mat)
-colnames(freq) <- data$SpeechDbId
-
-res <- wordfish(freq, dir=c(1,2))
+# actually doing stuff here
+data <- read_speeches('data/database_export_search_89.csv')
+d_6 <- filter_period(data, 6)
+mat <- get_frequency_matrix(d_6, 0.99)
+res <- wordfish(mat, dir=c(1,2))
---
+++ preprocessing_functions.R
@@ -0,0 +1,39 @@
+# Title     : Preprocessing Functions
+# Objective : One-Stop-Shop, execute to have all functions you might need for preprocessing
+# Created by: lukas
+# Created on: 14.05.2020
+
+read_speeches <- function(filepath) {
+  library(readr)
+  col_names <- c('Index', 'SpeechDbId', 'Date', 'Period', 'Sitting', 'DocDbId', 'Speaker', 'Party', 'InterjectionCount', 'InterjectionContent', 'ParagraphCount', 'Speech')
+  cols <- cols(Index=col_integer(),
+               SpeechDbId=col_integer(),
+               Date=col_date(),
+               Period=col_integer(),
+               Sitting=col_integer(),
+               DocDbId=col_integer(),
+               Speaker=col_character(),
+               Party=col_character(),
+               InterjectionCount=col_integer(),
+               InterjectionContent=col_character(),
+               ParagraphCount=col_integer(),
+               Speech=col_character())
+    return(read_csv(filepath, skip=1, col_names=col_names, col_types=cols))
+}
+
+filter_period <- function(dataset, period) {
+  return(dataset[dataset$Period == period,])
+}
+
+get_frequency_matrix <- function(dataset, sparse=0.999) {
+  library(tm)
+  corpus <- VCorpus(VectorSource(dataset$Speech))
+  corpus <- tm_map(corpus, content_transformer(tolower)) # MAKES EVERYTHING LOWERCASE
+  corpus <- tm_map(corpus, removeNumbers) # REMOVE NUMBERS
+  corpus <- tm_map(corpus, stripWhitespace) # REMOVE EXTRA WHITE SPACE
+  freq_mat <- TermDocumentMatrix(corpus)
+  sparce_mat <- removeSparseTerms(freq_mat, sparse)
+  freq <- as.matrix(sparce_mat)
+  colnames(freq) <- dataset$SpeechDbId
+  return(freq)
+}
